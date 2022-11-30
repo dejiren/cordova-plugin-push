f01df77460b1aabd8f95b106d3b35ac9eff27bd4
Kiriyama <kiriyama.k@wingarc.com>
2022-11-30 20:56:26 +0900

    誤って消したimport文を復活

--- src/android/com/adobe/phonegap/push/PushPlugin.kt
+++ src/android/com/adobe/phonegap/push/PushPlugin.kt
@@ -7,6 +7,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.ContentResolver
 import android.content.Context
+import android.content.Intent
 import android.content.res.Resources.NotFoundException
 import android.media.AudioAttributes
 import android.net.Uri
