060d9ca75d5b09b4cacbc3dcc4e962f93d62597c
Satomi Nakamura <osnakamura.sa@wingarc.com>
2022-12-05 11:46:06 +0900

    NotificationServiceの設定をcordova-plugin-pushに追加（jsはcordova-plugin-openwithより流用）

---
+++ src/ios/NotificationExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  notificationExt
+//
+//  Created by dejiren-user on 2022/12/05.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
